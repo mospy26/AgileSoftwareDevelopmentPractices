f54a78933ae43ae2037d89b7c76af366abab641e
Brendon <blam6948@uni.sydney.edu.au>
2018-09-10 20:40:51 +1000

    Added 2 test cases with 2 decimal places and adding negative/positive

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -21,12 +21,28 @@ class CalculatorTest {
 		double result = calc.add(64.5, 84.7);
 		assertEquals(result, 149.2);
 	}
-	
+
 	@Test
 	public void addTwoNegative() {
 		Calculator calc = new Calculator();
 
 		double result = calc.add(-45.0, -78.9);
 		assertEquals(result, -123.9);
+	}
+
+	@Test
+	public void addNegativePositive() {
+		Calculator calc = new Calculator();
+
+		double result = calc.add(420, -1234);
+		assertEquals(result, -814);
+	}
+
+	@Test
+	public void addTwoDecimalPlaces() {
+		Calculator calc = new Calculator();
+
+		double result = calc.add(150.52, 150.21);
+		assertEquals(result, 300.73);
 	}
 }
