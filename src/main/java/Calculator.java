7d3a6b942e93a03dce5fe1fb14322536ace029cb
Jesse <jpur5895@uni.sydney.edu.au>
2018-09-18 19:11:10 +1000

    Added command line input for addition, multiplication, and subtraction with input validation and test cases

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,69 @@
+import java.util.Scanner;
+import javafx.util.Pair;
+
 class Calculator {
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		System.out.println("Calculator usage: a OPERATOR b (e.g. 5 + 2)");
+
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("Input: ");
+			String s = scanner.nextLine();
+			
+			Pair<Integer, Double> result = calc.parseInput(s);
+			if (result.getKey() == 0) {
+				System.out.println(s + " = " + result.getValue());
+			} else {
+				System.out.println("Invalid input. Try again.");
+			}
+		}
+	}
 
 	public double add(double a, double b) {
 		return a + b;
 	}
 
-  	//Multiply method : Multiply a by b
 	public double multiply(double a, double b) {
 		return a * b;
 	}
 
-	//Subtraction method : Subtract a from b
 	public double subtraction(double a , double b){
 		return a - b;
 	}
+
+	// Returns pair of status code (0 if success) and result (if applicable)
+	public Pair<Integer, Double> parseInput(String s) {
+		double result = 0;
+
+		String[] parms = s.split(" ");
+		if (parms.length != 3) {
+			return new Pair<Integer, Double>(1, result);
+		}
+
+		double a, b;
+		try {
+			a = Double.parseDouble(parms[0]);
+			b = Double.parseDouble(parms[2]);
+		} catch (NumberFormatException e) {
+			return new Pair<Integer, Double>(1, result);
+		}
+
+		switch (parms[1]) {
+			case "+":
+				result = add(a, b);
+				break;
+			case "-":
+				result = subtraction(a, b);
+				break;
+			case "*":
+				result = multiply(a, b);
+				break;
+			default:
+				return new Pair<Integer, Double>(1, result);
+		}
+
+		return new Pair<Integer, Double>(0, result);
+	}
+
 }
